6aabcab5a7d35a0aaf1feb7d68d31572fe5a60b8
Jorge Alejandro <jorgealejandro1999@gmail.com>
2021-10-19 10:31:09 -0600

    Prints

--- MateriaCondensada/Proyecto/Main/Classes.py
+++ MateriaCondensada/Proyecto/Main/Classes.py
@@ -1,8 +1,20 @@
-#   CLASSES
+#-----------------------------------------------------------------------------------------#
+#---------------------------------------   CLASES   --------------------------------------#
+#-----------------------------------------------------------------------------------------#
+#   Author: Jorge Alejandro Rodriguez Aldana                                              #
+#   Date: 09/2021                                                                         #
+#   File: Classes.py                                                                      #
+#-----------------------------------------------------------------------------------------#
 
-#   Packages
 
-#   Code
+#-----------------------------------------------------------------------------------------#
+#                                        Packages                                         #
+#-----------------------------------------------------------------------------------------#
+
+
+#-----------------------------------------------------------------------------------------#
+#                                          Code                                           #
+#-----------------------------------------------------------------------------------------#
 class Particle:
     def __init__(self,ID,radius,r,v,a,m,q):
         self.ID = ID
@@ -12,3 +24,16 @@ class Particle:
         self.aceleration = a
         self.mass = m
         self.charge = q
+
+    def PrintData(self, data):
+        switch = {
+            'ID': print(self.ID),
+            'radious': print(self.radius),
+            'position': print(self.position),
+            'velocity': print(self.velocity),
+            'aceleration': print(self.aceleration),
+            'mass': print(self.mass),
+            'charge': print(self.charge)
+        }
+
+        return switch.get(data, 'Valid inputs are: ID, radious, position, velocity, aceleration, mass, charge')
