8d9c11055a8c09ab48f46f14fd8f0631dea9aef3
Jorge Alejandro <jorgealejandro1999@gmail.com>
2021-11-07 22:27:07 -0600

    Almost done

--- MateriaCondensada/Proyecto/Main/Random.py
+++ MateriaCondensada/Proyecto/Main/Random.py
@@ -21,21 +21,23 @@ import numpy as np
 # c = input("Cantidad de datos [columnas]: ")
 # A = input("Amplitud máxima: ")
 
-f = 20
-c = 10 # Para 2D, 13 para 3D
+f = 5
+c = 9 # Para 2D, 13 para 3D
 A = 10
 
 fila = []
 
 def rnd():
-    return np.power(int(A),random.random())
+    return format(np.power(int(A),random.random()),".2f")
 
 for i in range(int(f)):
-    for j in range(int(c)):
+    fila.append("1, 1,")
+    for j in range(int(c)-1):
         # fila.append(',')
-        fila.append(rnd())
+        fila.append(str(rnd())+",")
         # print(rnd())
+    fila.append(rnd())
     # print(i,',')
-    print(i,*fila)
+    print(str(i)+",",*fila)
     fila = []
 
